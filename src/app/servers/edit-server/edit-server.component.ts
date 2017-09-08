53407ce82a50badb632b78072f76dd3d1ca29290
sparshturkane <rajaturkane99@gmail.com>
2017-09-08 13:30:49 +0530

    125 retiving queryParams , fragment

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Route, ActivatedRoute } from "@angular/router";
 import { ServersService } from '../servers.service';
+
 
 @Component({
   selector: 'app-edit-server',
@@ -12,9 +13,11 @@ export class EditServerComponent implements OnInit {
   serverName = '';
   serverStatus = '';
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,private route: ActivatedRoute) { }
 
   ngOnInit() {
+    console.log(this.route.snapshot.queryParams);
+    console.log(this.route.snapshot.fragment);
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
