51efcd17886c90b340e1e13df83218f7876889a3
sparshturkane <rajaturkane99@gmail.com>
2017-09-08 12:08:21 +0530

    119 using relative path via programatic navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import { Routes, RouterModule } from '@angular/router';
 const appRoutes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'users', component: UsersComponent},
-  { path: 'servers', component: ServerComponent},
+  { path: 'servers', component: ServersComponent},
 ];
 @NgModule({
   declarations: [
